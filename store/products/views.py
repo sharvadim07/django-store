7538befeae9761e9e04395556ad1eb6b88a5d796
Vadim <sharvadim07@yandex.ru>
2023-05-17 13:05:55 +0700

    Add dynamic content

--- store/products/views.py
+++ store/products/views.py
@@ -3,8 +3,42 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "products/index.html")
+    context = {
+        "title": "Super store",
+        "username": "Vadim",
+    }
+    # context = {
+    #     "title": "Super store",
+    #     "username": "Vadim",
+    #     "is_promotion": True,
+    # }
+    return render(
+        request,
+        "products/index.html",
+        context=context
+    )
 
 
 def products(request):
-    return render(request, "products/products.html")
+    context = {
+        "title": "Store - Каталог",
+        "products": [
+            {
+                "image": "/static/vendor/img/products/Adidas-hoodie.png",
+                "name": "Худи черного цвета с монограммами adidas Originals",
+                "price": 6090,
+                "description": "Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.",
+            },
+            {
+                "image": "/static/vendor/img/products/Blue-jacket-The-North-Face.png",
+                "name": "Синяя куртка The North Face",
+                "price": 23725,
+                "description": "Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.",
+            },
+        ]
+    }
+    return render(
+        request,
+        "products/products.html",
+        context=context
+    )
