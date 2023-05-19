be4021091d190dc1889022b388097e1f66988cfd
sharvadim07 <sharvadim07@yandex.ru>
2023-05-19 19:33:10 +0700

    Add db to git

--- insert_some_products.sql
+++ insert_some_products.sql
@@ -1,5 +1,6 @@
 INSERT INTO products_product (name, image, description, short_description, price, quantity)
-VALUES ("Синяя куртка The North Face", "products_images/Blue-jacket-The-North-Face.png", "Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.", "", 23725.00, 1),
+VALUES ("Худи черного цвета с монограммами adidas Originals", "products_images/Adidas-hoodie.png", "Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.", "", 23725.00, 1),
+       ("Синяя куртка The North Face", "products_images/Blue-jacket-The-North-Face.png", "Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.", "", 23725.00, 1),
        ("Коричневый спортивный oversized-топ ASOS DESIGN", "products_images/Brown-sports-oversized-top-ASOS-DESIGN.png", "Материал с плюшевой текстурой. Удобный и мягкий.", "", 3390.00, 5),
        ("Черный рюкзак Nike Heritage", "products_images/Black-Nike-Heritage-backpack.png", "Плотная ткань. Легкий материал.", "", 2340.00, 10),
        ("Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex", "products_images/Black-Dr-Martens-shoes.png", "Гладкий кожаный верх. Натуральный материал.", "", 13590.00, 2);
