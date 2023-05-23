dbba2e846193101bce80930aa771c82e9132edd5
sharvadim07 <sharvadim07@yandex.ru>
2023-05-23 17:03:31 +0700

    Modify db

--- insert_some_products.sql
+++ insert_some_products.sql
@@ -4,3 +4,11 @@ VALUES ("Худи черного цвета с монограммами adidas O
        ("Коричневый спортивный oversized-топ ASOS DESIGN", "products_images/Brown-sports-oversized-top-ASOS-DESIGN.png", "Материал с плюшевой текстурой. Удобный и мягкий.", "", 3390.00, 5),
        ("Черный рюкзак Nike Heritage", "products_images/Black-Nike-Heritage-backpack.png", "Плотная ткань. Легкий материал.", "", 2340.00, 10),
        ("Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex", "products_images/Black-Dr-Martens-shoes.png", "Гладкий кожаный верх. Натуральный материал.", "", 13590.00, 2);
+
+
+INSERT INTO products_category (name)
+VALUES ("Новинки"),
+       ("Одежда"),
+       ("Обувь"),
+       ("Аксессуары"),
+       ("Подарки"),
