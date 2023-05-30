cc27e6737e466870f2d12e640355ba2924d10793
Vadim <sharvadim07@yandex.ru>
2023-05-30 13:10:18 +0700

    Custom authorisation forms

--- store/users/forms.py
+++ store/users/forms.py
@@ -1,8 +1,29 @@
 from django.contrib.auth.forms import AuthenticationForm
-
+from django import forms
 from users.models import User
 
+
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control py-4",
+                "id": "inputEmailAddress",
+                "type": "text",
+                "placeholder": "Введите имя пользователя",
+            }
+        ),
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control py-4",
+                "id": "inputPassword",
+                "type": "password",
+                "placeholder": "Введите пароль",
+            }
+        ),
+    )
     class Meta:
         model = User
         fields = ("username", "password")
