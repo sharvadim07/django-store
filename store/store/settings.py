39c05c3ccb3f90d5d2f2dcfd4cea3b5a06f4e15c
sharvadim07 <sharvadim07@yandex.ru>
2023-09-05 17:51:06 +0700

    Add user email verification (dummy)

--- store/store/settings.py
+++ store/store/settings.py
@@ -134,3 +134,8 @@ AUTH_USER_MODEL = "users.User"
 LOGIN_URL = "/users/login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+# Sending Emails
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_HOST = "95.188.74.83"
+# EMAIL_PORT = 587
--- store/users/models.py
+++ store/users/models.py
@@ -1,7 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.mail import send_mail
 
 
 # Create your models here.
 class User(AbstractUser):
     image = models.ImageField(upload_to="users_images", null=True, blank=True)
+    is_verified_email = models.BooleanField(default=False)
+
+
+class EmailVerification(models.Model):
+    code = models.UUIDField(unique=True)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expiration = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return f"EmailVerification object for {self.user.email}"
+
+    def send_verification_email(self):
+        send_mail(
+            "Subject here",
+            "Test email verification.",
+            "from@example.com",
+            [self.user.email],
+        )
