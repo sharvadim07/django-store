980ab7242dafa28aff9ad64134075419713699a6
Vadim <sharvadim07@yandex.ru>
2023-09-12 20:25:09 +0700

    FIX

--- store/products/tests.py
+++ store/products/tests.py
@@ -1,14 +1,14 @@
+import os
+
+from django import setup
 from django.test import TestCase
 from django.urls import reverse
-from django import setup
-import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "store.settings")
 setup()
 
 
 class IndexViewTestCase(TestCase):
-
     def test_view(self):
         path = reverse("index")
         response = self.client.get(path=path)
