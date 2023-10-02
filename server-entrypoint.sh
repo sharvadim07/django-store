fab5f3af85beff766cc506d7c1c984b13aa7243e
root <root@home-server>
2023-10-02 14:48:24 +0700

    Add waiting db in server entrypoint

--- server-entrypoint.sh
+++ server-entrypoint.sh
@@ -6,11 +6,11 @@ do
 done
 
 
-# until python manage.py migrate
-# do
-#     echo "Waiting for db to be ready..."
-#     sleep 2
-# done
+until python manage.py migrate
+do
+    echo "Waiting for db to be ready..."
+    sleep 2
+done
 
 
 python manage.py collectstatic --noinput
